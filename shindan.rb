13a0755db9049b3473c84040e7bb65483d936afc
ikaruga <ikaruga777@gmail.com>
2016-11-19 01:01:53 +0900

    オプションつけてやる

--- shindan.rb
+++ shindan.rb
@@ -1,21 +1,24 @@
-
-
-exit if ARGV[0].nil?
-
-# Nokogiriライブラリの読み込み
+require 'optparse'
 require 'nokogiri'
 require 'mechanize'
 
+def shindan (id,input)
+  mechanize = Mechanize.new
+  mechanize.user_agent_alias= "Mac Safari 4"
+  url = "https://shindanmaker.com/#{id}"
 
-# スクレイピング先のURL
-url = 'https://shindanmaker.com/167854'
-
-mechanize = Mechanize.new
-mechanize.user_agent_alias= "Mac Safari 4"
-mechanize.get(url) do |page|
-  mypage = page.form_with( name: "enter") do |form|
-    form.u = ARGV[0]
-  end.submit
-  doc = Nokogiri::HTML(mypage.content.toutf8)
-  puts doc.xpath('string(//div[@class="result2"])').strip
+  mechanize.get(url) do |page|
+    mypage = page.form_with( name: "enter") do |form|
+      form.u = input
+    end.submit
+    doc = Nokogiri::HTML(mypage.content.toutf8)
+    return doc.xpath('string(//div[@class="result2"])').strip
+  end
 end
+
+opts = OptionParser.new
+options = {:id => "167854", :str => "i"}
+opts.on("-i","--id=id"){|v| options[:id] = v}
+opts.on("-s","--str=string"){|s| options[:str] = s}
+opts.parse(ARGV)
+puts shindan(options[:id],options[:str])
