2178c01f97ae93c1471decdcb99425217577b425
ikaruga <ikaruga777@gmail.com>
2016-12-27 08:24:11 +0900

    クラスにして実行ファイル作る

--- lib/shindan.rb
+++ lib/shindan.rb
@@ -2,24 +2,36 @@ require 'optparse'
 require 'nokogiri'
 require 'mechanize'
 
-def shindan (id,input)
-  mechanize = Mechanize.new
-  mechanize.user_agent_alias= "Mac Safari 4"
-  url = "https://shindanmaker.com/#{id}"
+class Shindan
+  def initialize(argv)
+    @argv = argv
+  end
 
-  mechanize.get(url) do |page|
-    mypage = page.form_with( name: "enter") do |form|
-      form.u = input
-    end.submit
-    doc = Nokogiri::HTML(mypage.content.toutf8)
-    return doc.xpath('string(//div[@class="result2"])').strip
+  def parse_options
+    opts = OptionParser.new @argv
+    options = {:id => "167854", :str => "i"}
+    opts.on("-i","--id=id"){|v| options[:id] = v}
+    opts.on("--LArc"){|v| options[:id] = "334680"}
+    opts.on("-s","--str=string"){|s| options[:str] = s}
+    opts.parse(ARGV)
+    options
+  end
+
+  def shindan (id,input)
+    mechanize = Mechanize.new
+    mechanize.user_agent_alias= "Mac Safari 4"
+    url = "https://shindanmaker.com/#{id}"
+
+    mechanize.get(url) do |page|
+      mypage = page.form_with( name: "enter") do |form|
+        form.u = input
+      end.submit
+      doc = Nokogiri::HTML(mypage.content.toutf8)
+      return doc.xpath('string(//div[@class="result2"])').strip
+    end
+  end
+  def main
+    options = parse_options()
+    puts shindan(options[:id],options[:str])
   end
 end
-
-opts = OptionParser.new
-options = {:id => "167854", :str => "i"}
-opts.on("-i","--id=id"){|v| options[:id] = v}
-opts.on("--LArc"){|v| options[:id] = "334680"}
-opts.on("-s","--str=string"){|s| options[:str] = s}
-opts.parse(ARGV)
-puts shindan(options[:id],options[:str])
